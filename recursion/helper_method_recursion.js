f0978ecaf8d422033530d67a9bf2ede43a369e39
Oluwafemi Anjorin <leelsmuth@Oluwafemis-MacBook-Pro.local>
2022-02-04 20:58:52 -0500

    recursion---ish

---
+++ recursion/helper_method_recursion.js
@@ -0,0 +1,24 @@
+// Collect odd value in arrays
+
+function collectOddValues(arr) {
+
+  let result = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) {
+      return;
+    }
+
+    if (helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
+    console.log(helperInput);
+    helper(helperInput.slice(1));
+  }
+
+  helper(arr);
+
+  return result;
+}
+
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
