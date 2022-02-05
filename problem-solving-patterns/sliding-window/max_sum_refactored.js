7f17de64790007ab72f6bf891bc6325cb3e32464
Oluwafemi Anjorin <leelsmuth@Oluwafemis-MacBook-Pro.local>
2022-02-04 19:34:50 -0500

    add description

--- problem-solving-patterns/sliding-window/max_sum_refactored.js
+++ problem-solving-patterns/sliding-window/max_sum_refactored.js
@@ -1,4 +1,14 @@
-function maxSubarraySum(arr, num){
+// Given an array of intergers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+// Note that a subarray must consist of a consecutive elements from the original array. In the first example below [100, 200, 300] is a subarray of the original array, but [100, 300] is not
+
+// maxSubarraySum([100, 200, 300, 400], 2) ... 700
+// maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4) ... 39
+// maxSubarraySum([-3, 4, 0, -2, 6, -1], 2) ... 5
+// maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2) ... 5
+// maxSubarraySum([2,3], 3) ... null
+
+function maxSubarraySum(arr, num) {
   let maxSum = 0;
   let tempSum = 0;
   if (arr.length < num) return null;
@@ -13,4 +23,4 @@ function maxSubarraySum(arr, num){
   return maxSum;
 }
 
-maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
